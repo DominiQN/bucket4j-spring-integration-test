3c47930a2c9db86d7fea873d6d301fa0a779961c
Nam Dongmin <nam.dongmin@htbeyond.com>
2025-02-17 14:16:44 +0900

    Add project

---
+++ src/main/kotlin/dev/dominiqn/bucket4j/Bucket4jApplication.kt
@@ -0,0 +1,11 @@
+package dev.dominiqn.bucket4j
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Bucket4jApplication
+
+fun main(args: Array<String>) {
+    runApplication<Bucket4jApplication>(*args)
+}
---
+++ src/test/kotlin/dev/dominiqn/bucket4j/Bucket4jApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.dominiqn.bucket4j
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Bucket4jApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
